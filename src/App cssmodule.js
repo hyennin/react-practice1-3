23e7cdf27092528045e1fdc6d0947d72f5ea97c7
hyennin <chm050202@gmail.com>
2023-05-24 14:09:51 +0900

    styled-components 실습

---
+++ src/App cssmodule.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import CSSModule from "./CSSModule";
+import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CSSModule />
+      </div>
+    );
+  }
+}
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { Component } from "react";
-import CSSModule from "./CSSModule";
-import SassComponent from "./SassComponent";
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <CSSModule />
-      </div>
-    );
-  }
+const SimpleButton = styled.button`
+  color: white;
+  background: green;
+`;
+
+// SimpleButton을 상속받고 추가적으로 font-size를 설정함
+const LargeButton = styled(SimpleButton)`
+  font-size: 50px;
+`;
+
+const App = () => {
+  return (
+    <div>
+      {/* 렌더링이 될 경우에 클래스 이름을 자동적으로 붙여줌 */}
+      <SimpleButton>Simple</SimpleButton>
+      <LargeButton>Large</LargeButton>
+    </div>
+  )
 }
+
 export default App;
